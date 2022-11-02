3fb8552a4a2034ad4ac7aec1460e1a97790a0787
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-02 20:28:34 +0100

    Initial Commit

---
+++ GetActive/GetActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetActiveApp.swift
+//  GetActive
+//
+//  Created by Igor Łopatka on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct GetActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
