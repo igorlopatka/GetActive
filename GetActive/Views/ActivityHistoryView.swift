12b886c907f971ac0d41b8d58648b409dbaa7c52
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-07 16:07:11 +0100

    Create rest view files

---
+++ GetActive/Views/ActivityHistoryView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivitiesHistoryView.swift
+//  GetActive
+//
+//  Created by Igor Łopatka on 07/11/2022.
+//
+
+import SwiftUI
+
+struct ActivityHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityHistoryView()
+    }
+}
