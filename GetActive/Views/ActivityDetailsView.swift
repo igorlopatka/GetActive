9d28026eb57f1cca2630464886946b60b9688b90
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-07 16:03:11 +0100

    Create ActivityDetailsView

---
+++ GetActive/Views/ActivityDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityDetailsView.swift
+//  GetActive
+//
+//  Created by Igor Łopatka on 07/11/2022.
+//
+
+import SwiftUI
+
+struct ActivityDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityDetailsView()
+    }
+}
