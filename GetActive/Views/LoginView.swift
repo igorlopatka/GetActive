8f434b28157ce31807e343bf7f9f123514c3cc3c
Igor Łopatka <igor.lopatka11@gmail.com>
2022-11-10 16:29:04 +0100

    Create login/password textfields for authentication views

--- GetActive/Views/LoginView.swift
+++ GetActive/Views/LoginView.swift
@@ -8,13 +8,62 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @EnvironmentObject var viewRouter: ViewRouter
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            SignInCredentialFields(email: $email, password: $password)
+            Button(action: {
+                //Sign in user using Firebase
+            }) {
+                Text("Log In")
+                    .bold()
+                    .frame(width: 360, height: 50)
+                    .background(.thinMaterial)
+                    .cornerRadius(10)
+            }
+            Spacer()
+            HStack {
+                Text("Don't have an account?")
+                Button(action: {
+                    viewRouter.currentPage = .signUpPage
+                }) {
+                    Text("Sign Up")
+                }
+            }
+            .opacity(0.9)
+        }
+        .padding()
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
+    }
+}
+
+struct SignInCredentialFields: View {
+    
+    @Binding var email: String
+    @Binding var password: String
+    
+    var body: some View {
+        Group {
+            TextField("Email", text: $email)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .textInputAutocapitalization(.never)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .padding(.bottom, 30)
+        }
     }
 }
--- GetActive/Views/RegistrationView.swift
+++ GetActive/Views/RegistrationView.swift
@@ -8,13 +8,70 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    
+    @EnvironmentObject var viewRouter: ViewRouter
+    
+    @State private var email = ""
+    @State private var password = ""
+    @State private var passwordConfirmation = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) {
+            SignUpCredentialFields(email: $email,
+                                   password: $password,
+                                   passwordConfirmation: $passwordConfirmation)
+            Button {
+                // authenticate
+            } label: {
+                Text("Sign Up")
+                    .bold()
+                    .frame(width: 360, height: 50)
+                    .background(.thinMaterial)
+                    .cornerRadius(10)
+            }
+            Spacer()
+            HStack {
+                Text("Already have an account?")
+                Button(action: {
+                    viewRouter.currentPage = .signInPage
+                }) {
+                    Text("Log In")
+                }
+            }
+            .opacity(0.9)
+        }
     }
 }
 
 struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
         RegistrationView()
+    }
+}
+
+struct SignUpCredentialFields: View {
+    
+    @Binding var email: String
+    @Binding var password: String
+    @Binding var passwordConfirmation: String
+    
+    var body: some View {
+        Group {
+            TextField("Email", text: $email)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .textInputAutocapitalization(.never)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+            SecureField("Confirm Password", text: $passwordConfirmation)
+                .padding()
+                .background(.thinMaterial)
+                .cornerRadius(10)
+                .border(Color.red, width: passwordConfirmation != password ? 1 : 0)
+                .padding(.bottom, 30)
+        }
     }
 }
